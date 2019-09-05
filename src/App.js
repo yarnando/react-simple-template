e13a581e58b298e018846c486ad89db470c1aca4
yarnando <fernandosilva.smsrio@gmail.com>
2019-09-05 18:15:38 -0300

    Modal component ok

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
-import React, { Component } from 'react';
-import Header from './components/shared/header'
+import React, { Component } from "react";
+import Header from "./components/shared/header";
+import Modal from "./components/shared/modal";
 
 class App extends Component {
 
-    state = {
-        navItems: [
-            { type:"routerlink", link:"/test0", title: "title0" },
-            { type:"routerlink", link:"/test1", title: "title1" },
-            { type:"anchor", link:"/test2", title: "title2" },           
-        ]        
+    constructor() {
+        super()
+        this.closeModal = this.closeModal.bind(this);
     }
 
-    render() {
+  state = {
+    error: false,
+    success: false,
+    simple: false,
+    loading: false,
+    navItems: [
+      { type: "routerlink", link: "/test0", title: "title0" },
+      { type: "routerlink", link: "/test1", title: "title1" },
+      { type: "anchor", link: "/test2", title: "title2" }
+    ]
+  };
 
-        return (
-            <Header title="Template" navItems={this.state.navItems}/>
-        );
-    } 
+  closeModal(field) {
+    this.setState({ [field]: false })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header title="Template" navItems={this.state.navItems} />
+        <div className="container">
+          <div className="row">
+            <div className="grid-item-2">
+                <button onClick={() => this.setState({ simple: !this.state.simple })}>
+                Toggle simple
+                </button>
+                <Modal
+                showing={this.state.simple}
+                close={() => this.closeModal('simple')}
+                >
+                    <span>Simple Message</span>
+                </Modal>
+            </div>            
+            <div className="grid-item-2">
+                <button onClick={() => this.setState({ success: !this.state.success })}>
+                Toggle success
+                </button>
+                <Modal
+                type="success"
+                showing={this.state.success}
+                close={() => this.closeModal('success')}
+                >
+                    <span>Success Message</span>
+                </Modal>                
+            </div>            
+            <div className="grid-item-2">
+                <button onClick={() => this.setState({ error: !this.state.error })}>
+                Toggle error
+                </button>
+                <Modal
+                type="error"
+                showing={this.state.error}
+                close={() => this.closeModal('error')}
+                >
+                    <span>Error Message</span>
+                </Modal>                
+            </div>            
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/shared/modal/index.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+export default class Modal extends Component {
+
+    state = {
+        statusClasses: "slideFade-on",
+        showingContainer: false,
+        modalShowing: false,
+        modalType: {}
+    };
+
+    componentDidMount() {
+        this.modalType();
+    }
+
+    modalType() {
+        if(this.props.type == 'error') {
+            this.setState( {modalType: { type: 'error', icon: 'icon-cross'} })
+        } else if(this.props.type == 'success') {
+            this.setState( {modalType: { type: 'success', icon: 'icon-checkmark'} })
+        } else if(this.props.type == 'loading') {
+            this.setState( {modalType: { type: 'loading', icon: 'icon-spinner11'} })
+        } else {
+            this.setState( {modalType: { type: 'default', icon: ''} })
+        }
+    }    
+
+    showModal() {
+        //wait for the background appear (this.props.showing == true) and set modalShowing true, making the transition execute
+        //called on componentDidUpdate
+        setTimeout(() => {
+            this.setState({ modalShowing: true });
+        }, 100);
+    }
+
+    //wait for the modal to disappear and hides background, called on modal button "close"
+    hideModal() {
+        this.setState({ modalShowing: false });
+        setTimeout(() => {
+            this.props.close();
+        }, 700);
+    }
+
+    componentDidUpdate(oldProps) {
+        const newProps = this.props;
+        if (newProps.showing !== oldProps.showing && newProps.showing == true)
+            this.showModal();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.showing && (
+                    <div className="modalContainer">
+                        <div
+                            className={`msgBox ${
+                                this.state.modalShowing ? "slideFade-on" : "slideFade-off"
+                                }`}
+                        >
+                            <div className={`${this.state.modalType.type}MsgText`}>
+                                <span className={`${this.state.modalType.icon} iconElement`}></span>
+                                {this.props.children}
+                            </div>
+                            <button onClick={() => this.hideModal()}> Close</button>
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
